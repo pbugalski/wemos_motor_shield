554120f26c93def8f8cc877f634535a1892e4b3b
Nathan Phillips <nathan.phillips@diffblue.com>
2017-11-22 11:55:29 +0000

    Added comment and changed style to improve clarity

--- src/main.c
+++ src/main.c
@@ -4,63 +4,84 @@
 
 #define MODE_IN                 0x00
 #define MODE_OUT                0x01
-#define MODE_AF                 0x02
-#define MODE_AN                 0x03
+#define MODE_AF                 0x02        // Alternate function, e.g. USAT, SPI
+#define MODE_AN                 0x03        // Analog mode
 #define MODER(mode, pin)        ((mode) << (2 * (pin)))
 
 volatile uint32_t timeout = 0;
 
 void SysTick_Handler(void)
 {
-    if (timeout)
-        timeout--;
+    if (timeout != 0)
+        --timeout;
 }
 
 int receive_cmd(uint8_t *buf, uint16_t count)
 {
     int i;
 
+    // Clear the control register and wait until peripheral enable reports as cleared
     I2C1->CR1 = 0;
-    while (I2C1->CR1 & I2C_CR1_PE);
+    while ((I2C1->CR1 & I2C_CR1_PE) != 0) {}
 
+    // Set peripheral enable and wait until it reports as set
     I2C1->CR1 = I2C_CR1_PE;
-    while ((I2C1->CR1 & I2C_CR1_PE) == 0);
+    while ((I2C1->CR1 & I2C_CR1_PE) == 0) {}
 
+    // Clear all interrupts
     I2C1->ICR = 0xffffffff;
 
-    while ((I2C1->ISR & I2C_ISR_ADDR) == 0);
+    // Wait for address matched (slave mode) interrupt
+    while ((I2C1->ISR & I2C_ISR_ADDR) == 0) {}
+    // Clear this interrupt
     I2C1->ICR = I2C_ICR_ADDRCF;
 
     if (I2C1->ISR & I2C_ISR_DIR) {
-        // read - not supported
+        // Read is requested but we don't support it
         return -1;
     }
 
+    // Allow 4 ticks to receive all data and stop
     timeout = 4;
+
     for (i = 0; i < count; i++) {
-        while (((I2C1->ISR & I2C_ISR_RXNE) == 0) && (timeout));
-        if (!timeout)
-            return -2;
+        // Wait until receive data register is empty
+        while ((I2C1->ISR & I2C_ISR_RXNE) == 0) {
+            if (timeout == 0)
+                return -2;
+        }
+        // Save the received data
         *buf++ = I2C1->RXDR;
     }
 
-    while (((I2C1->ISR & I2C_ISR_STOPF) == 0) && (timeout));
-    if (!timeout)
-        return -2;
-
+    // Wait until get stop interrupt, fail if don't get within the timeout
+    while ((I2C1->ISR & I2C_ISR_STOPF) == 0) {
+        if (timeout == 0)
+            return -2;
+    }
+    // Clear the stop interrupt
     I2C1->ICR = I2C_ICR_STOPCF;
+
+    // Success
     return 0;
 }
 
 int main()
 {
+    // Enable GPIOA clock for the AHB peripheral clock
     RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+    // Timer 3 and I2C1 clock enable in the APB1 peripheral clock
     RCC->APB1ENR |= RCC_APB1ENR_I2C1EN | RCC_APB1ENR_TIM3EN;
 
-    GPIOA->MODER |= MODER(MODE_OUT, PIN_AIN1) | MODER(MODE_OUT, PIN_AIN2) |
+    // Set modes for pins connected to GPIO A (i.e. most of them)
+    GPIOA->MODER |=
+        // Motor controls
+        MODER(MODE_OUT, PIN_AIN1) | MODER(MODE_OUT, PIN_AIN2) |
         MODER(MODE_OUT, PIN_BIN1) | MODER(MODE_OUT, PIN_BIN2) |
+        // I2C
         MODER(MODE_AF, PIN_SCL) | MODER(MODE_AF, PIN_SDA) |
         MODER(MODE_AF, PIN_PWMA) | MODER(MODE_AF, PIN_PWMB) |
+        // Driver chip standby
         MODER(MODE_OUT, PIN_STBY);
 
     GPIOA->AFR[0] |= (1 << GPIO_AFRH_AFRH6_Pos) | (1 << GPIO_AFRH_AFRH7_Pos);
@@ -69,7 +90,9 @@ int main()
     GPIOA->OTYPER |= GPIO_OTYPER_OT_9 | GPIO_OTYPER_OT_10;
     GPIOA->PUPDR |= GPIO_PUPDR_PUPDR9_0 | GPIO_PUPDR_PUPDR10_0;
 
-    I2C1->OAR1 = I2C_OAR1_OA1EN | 0x60;
+    // Set own I2C address to 0x30
+    I2C1->OAR1 = I2C_OAR1_OA1EN | (0x30 << 1);
+    // Enable I2C
     I2C1->CR1 = I2C_CR1_PE;
 
     TIM3->CCMR1 = TIM_CCMR1_OC1PE | TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_1 |
