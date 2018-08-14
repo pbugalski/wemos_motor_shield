5cf5d16970c6cc46341fcb4229e26891a038564f
danielfmo <danielfilipe.mo@gmail.com>
2018-08-14 23:41:25 +0200

    Fix set PWM frequency and resolution

--- src/user_i2c.c
+++ src/user_i2c.c
@@ -7,8 +7,8 @@ total 4bytes
 
 |0.5byte CMD| 3.5byte Parm|
 
-CMD								| 	parm
-0x0X  set freq  	|  uint32  freq
+CMD				  |  parm
+0x0X  set freq    |  uint32  freq
 0x10  set motorA  |  uint8 dir  uint16 pwm
 0x11  set motorB  |  uint8 dir  uint16 pwm
 */
@@ -21,22 +21,20 @@ void user_i2c_proc(uint8_t i2c_data[4])
     {
         case 0:
         {
-            uint32_t freq = (uint32_t)(i2c_data[0] & 0x0f) << 24 |
-                            (uint32_t)i2c_data[1] << 16 |
-                            (uint32_t)i2c_data[2] << 8 |
-                            (uint32_t)i2c_data[3];
+            uint32_t freq = (uint32_t)(i2c_data[0] & 0x0f) << 16 |
+                            (uint32_t)i2c_data[1] << 8 |
+                            (uint32_t)i2c_data[2];
             Set_Freq(freq);
             break;
         }
         case 1:
         {
             uint8_t motor = i2c_data[0] & 0x01;
-            uint8_t dir = i2c_data[1];
-            uint16_t pulse = (uint16_t)i2c_data[2] << 8 | (uint16_t)i2c_data[3];
+            uint8_t dir   = i2c_data[1];
+            uint8_t pulse = i2c_data[2];
 
             Set_TB6612_Dir(motor, dir, pulse);
             break;
         }
     }
 }
-
--- src/user_i2c.h
+++ src/user_i2c.h
@@ -1,8 +1,14 @@
 #ifndef __USER_I2C_H
 #define __USER_I2C_H
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #include <stdint.h>
 
 void user_i2c_proc(uint8_t i2c_data[4]);
 
+#ifdef __cplusplus
+}
+#endif
 #endif
